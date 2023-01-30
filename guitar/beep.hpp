7dd7332ebce818016877f22460cb2ae626afa9ba
黄军杰 <huangjj@friendcom.com>
2023-01-30 16:59:04 +0800

    ✨ feat(guitar): 初步新增beep类

---
+++ guitar/beep.hpp
@@ -0,0 +1,88 @@
+#ifndef BEEP_HPP
+#define BEEP_HPP
+/*
+ * *************************************************************************************************
+ * TTU-SG
+ * Copyright (C) 2022 Friendcom
+ * 
+ * @file         : beep.hpp
+ * @author       : ekko
+ * @version      : V0.1
+ * @date         : 2023-01-30
+ * @brief        : 
+ * @details      : 
+ * @attention    : 
+ * 
+ * Change History : 
+ * 
+ * 
+ * 
+ * *************************************************************************************************
+ */
+#include <stddef.h>
+
+#include <errno.h>
+#include <string.h>
+
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <fcntl.h>
+
+#include <linux/input.h>
+
+#include <string>
+#include <iostream>
+
+class Beep
+{
+public:
+    Beep(std::string dev = "/dev/input/by-path/platform-pcspkr-event-spkr")
+    {
+        dev_name = dev;
+    }
+    ~Beep()
+    {
+
+    }
+
+    bool open()
+    {
+        fd = ::open(dev_name.c_str(), O_WRONLY);
+        if (fd == -1) {
+            printf("could not open(2) %s: %s",
+                        dev_name, strerror(errno));
+            return false;
+        }
+        return true;
+    }
+
+    void close()
+    {
+        ::close(fd);
+    }
+
+    bool set_freq(int freq)
+    {
+        struct input_event e;
+
+        memset(&e, 0, sizeof(e));
+        e.type = EV_SND;
+        e.code = SND_TONE;
+        e.value = 0;
+
+        if (sizeof(e) != write(fd, &e, sizeof(e))) {
+            return true;
+        }
+
+        return false;
+    }
+    
+private:
+    std::string dev_name;
+    int fd = -1;
+};
+
+#endif /* BEEP_HPP */
--- guitar/main.cpp
+++ guitar/main.cpp
@@ -1,12 +1,73 @@
+/*
+ * *************************************************************************************************
+ * TTU-SG
+ * Copyright (C) 2022 Friendcom
+ * 
+ * @file         : main.cpp
+ * @author       : ekko
+ * @version      : V0.1
+ * @date         : 2023-01-30
+ * @brief        : 
+ * @details      : 
+ * @attention    : 
+ * 
+ * Change History : 
+ * 
+ * 
+ * 
+ * *************************************************************************************************
+ */
 #include <iostream>
+#include <thread>
 #include "ev3dev.h"
+#include "beep.hpp"
 
 using namespace std;
 
+/**
+ * @brief: 是否需要播放
+ * @param &touch
+ * @param &infrared
+ * @param &level 播放频率
+ * @retval: 
+ */
+bool need_play(ev3dev::touch_sensor &touch,ev3dev::infrared_sensor &infrared,int &level)
+{
+    //拨弦按下
+    if(!touch.is_pressed())
+    {
+        //获取距离
+        auto distance = infrared.proximity();
+        printf("distance : %d\n",distance);
+    }
+    return false;
+}
+
 int main(int argc, char** argv)
 {
-    ev3dev::infrared_sensor is;
+    //触摸传感器
+    ev3dev::touch_sensor touch;
+    //红外距离传感器
+    ev3dev::infrared_sensor infrared;
+    //中型电机
+    ev3dev::medium_motor motor;
+    //蜂鸣器
+    Beep beep;
+    while(!beep.open())
+    {
+        this_thread::sleep_for(chrono::seconds(1));
+    }
 
-    cout << "hello world!" << endl;
-    return 0;
+    int level = 0;
+    for(;;)
+    {
+        //需要播放
+        if(need_play(touch,infrared,level))
+        {
+            beep.set_freq(level);
+        }
+        else
+            beep.set_freq(0);
+        this_thread::sleep_for(chrono::milliseconds(100));
+    }
 }
