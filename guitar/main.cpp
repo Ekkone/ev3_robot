b2f0a3645a3165d1b4284390a6f23e022524f56a
黄军杰 <huangjj@friendcom.com>
2023-01-30 17:58:59 +0800

    ✨ feat(guitar): 优化结构

--- guitar/main.cpp
+++ guitar/main.cpp
@@ -1,7 +1,7 @@
 /*
  * *************************************************************************************************
  * TTU-SG
- * Copyright (C) 2022 Friendcom
+ * Copyright (C) 2022 
  * 
  * @file         : main.cpp
  * @author       : ekko
@@ -22,19 +22,36 @@
 #include "ev3dev.h"
 #include "beep.hpp"
 
-using namespace std;
-
-uint8_t music_list[] = {131,147,165,175,196,220,247,262};
+//
+// ┌─────┬─────────────────────────┐
+// │note │         octave          │
+// │name │  3     4     5      6   │
+// ├─────├─────────────────────────┤
+// │ C   │ 131   262    523   1047 │
+// │ C#  │ 139   277    554   1109 │
+// │ D   │ 147   294    587   1175 │
+// │ D#  │ 156   311    622   1245 │
+// │ E   │ 165   330    659   1319 │
+// │ F   │ 175   349    698   1397 │
+// │ F#  │ 185   370    740   1480 │
+// │ G   │ 196   392    784   1568 │
+// │ G#  │ 208   415    831   1661 │
+// │ A   │ 220   440    880   1760 │
+// │ A#  │ 233   466    932   1865 │
+// │ B   │ 247   494    988   1976 │
+// │ C   │ 262   523   1047   2093 │
+// └─────┴─────────────────────────┘
+int music_list[] = {131,147,165,175,196,220,247,262};
 
 /**
- * @brief: 是否需要播放
+ * @brief: 计算当前播放level
  * @param &touch
  * @param &infrared
- * @param &level 播放频率
  * @retval: 
  */
-bool need_play(ev3dev::touch_sensor &touch,ev3dev::infrared_sensor &infrared,int &level)
+int cal_level(ev3dev::touch_sensor &touch,ev3dev::infrared_sensor &infrared)
 {
+    int level = 0;
     //拨弦按下
     if(!touch.is_pressed())
     {
@@ -51,11 +68,15 @@ bool need_play(ev3dev::touch_sensor &touch,ev3dev::infrared_sensor &infrared,int
             level = 0;
         printf("level : %d\n",level);
     }
-    return false;
+    return level;
 }
 
-
-
+/**
+ * @brief: 
+ * @param argc
+ * @param argv
+ * @retval: 
+ */
 int main(int argc, char** argv)
 {
     //触摸传感器
@@ -68,20 +89,12 @@ int main(int argc, char** argv)
     Beep beep;
     while(!beep.open())
     {
-        this_thread::sleep_for(chrono::seconds(1));
+        sleep(1);
     }
-
-    int level = 0;
     for(;;)
     {
-        //需要播放
-        if(need_play(touch,infrared,level))
-        {
-            beep.set_freq(music_list[level]);
-        }
-        else
-            beep.set_freq(0);
+        beep.set_freq(music_list[cal_level(touch,infrared)]);
         
-        this_thread::sleep_for(chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
