a763e3d583823a91c4a22bd30f8ab6b29b462545
黄军杰 <huangjj@friendcom.com>
2023-01-30 17:50:37 +0800

    ✨ feat(guitar): 新增播放level计算

--- guitar/main.cpp
+++ guitar/main.cpp
@@ -24,6 +24,8 @@
 
 using namespace std;
 
+uint8_t music_list[] = {131,147,165,175,196,220,247,262};
+
 /**
  * @brief: 是否需要播放
  * @param &touch
@@ -36,12 +38,23 @@ bool need_play(ev3dev::touch_sensor &touch,ev3dev::infrared_sensor &infrared,int
     //拨弦按下
     if(!touch.is_pressed())
     {
-        //获取距离
+        //获取距离 0-100 :: 0-70cm
+        //3.5-40 -> 5-57
         auto distance = infrared.proximity();
         printf("distance : %d\n",distance);
+        //计算levle 0-55
+        level = (distance - 5) / 7;
+        //level超限处理
+        if(level >= sizeof(music_list))
+            level = sizeof(music_list);
+        else if(level < 0)
+            level = 0;
+        printf("level : %d\n",level);
     }
     return false;
 }
+
+
 
 int main(int argc, char** argv)
 {
@@ -64,10 +77,11 @@ int main(int argc, char** argv)
         //需要播放
         if(need_play(touch,infrared,level))
         {
-            beep.set_freq(level);
+            beep.set_freq(music_list[level]);
         }
         else
             beep.set_freq(0);
+        
         this_thread::sleep_for(chrono::milliseconds(100));
     }
 }
